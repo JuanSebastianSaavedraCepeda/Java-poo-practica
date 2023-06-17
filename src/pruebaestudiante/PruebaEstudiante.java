ae432acbf875f693523e07ed10fe221bf0dd1023
JuanSebastianSaavedraCepeda <63920150+JuanSebastianSaavedraCepeda@users.noreply.github.com>
2023-06-17 14:36:01 -0500

    creacion de clase principal

--- src/pruebaestudiante/PruebaEstudiante.java
+++ src/pruebaestudiante/PruebaEstudiante.java
@@ -1,20 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pruebaestudiante;
 
-/**
- *
- * @author Sebastian
- */
+import java.util.Scanner;
 public class PruebaEstudiante {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada =new Scanner(System.in);
+        
+        int total =0;
+        int contadorCalificaciones = 0; // inicializa # de calificaciones introducidas
+
+
+ // fase de procesamiento
+// pide entrada y lee calificación del usuario
+ System.out.print("Escriba la calificacion o -1 para terminar: ");
+ int calificacion = entrada.nextInt();
+
+ // itera hasta recibir el valor centinela del usuario
+ while (calificacion != -1)
+ {
+ total = total + calificacion; // suma calificacion al total
+contadorCalificaciones = contadorCalificaciones + 1; // incrementa el contador
+
+ // pide entrada y lee la siguiente calificación del usuario
+ System.out.print("Escriba la calificacion o -1 para terminar: ");
+ calificacion = entrada.nextInt();
+ {
+
+ // fase de terminación
+ // si el usuario introdujo al menos una calificación...
+ if (contadorCalificaciones != 0)
+ {
+ // usa número con punto decimal para calcular promedio de calificaciones
+ double promedio = (double) total / contadorCalificaciones;
+
+ // muestra total y promedio (con dos dígitos de precisión)
+ System.out.printf("%nEl total de las %d calificaciones introducidas es %d%n",
+ contadorCalificaciones, total);
+ System.out.printf("El promedio de la clase es %.2f%n", promedio);
+ }
+ else // no se introdujeron calificaciones, por lo que se muestra el mensaje apropiado
+ System.out.println("No se introdujeron calificaciones");
+        
+ }
+ }
     }
-    
 }
+ 
+    
+
